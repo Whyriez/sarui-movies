e949e4c7a10c271cc337e72cbb49f69200de990c
Alim Suma <sumaalim@gmail.com>
2024-08-24 23:21:35 +0800

    update navbar mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -51,7 +51,7 @@ function Navbar() {
                         </div>
                         <ul
                             tabIndex={0}
-                            className="menu menu-sm dropdown-content rounded-box z-[60] mt-3 w-52 p-2 shadow">
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[60] mt-3 w-52 p-2 shadow">
                             <li>
                                 <h1>Movies</h1>
                                 <ul className="p-2">
