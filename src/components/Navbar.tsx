8334bca0aa9b8a98f705806609f3f9af7c425735
Alim Suma <sumaalim@gmail.com>
2024-08-25 11:29:40 +0800

    fixed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,7 +32,7 @@ function Navbar() {
 
     return (
         <div >
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed top-0 z-50 bg-base-100">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
