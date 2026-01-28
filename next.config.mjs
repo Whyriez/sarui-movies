77225d6fb1d1de0447e4d11473e5afeeebe3fb3c
whyriez <sumaalim@gmail.com>
2026-01-28 16:15:44 +0800

    update listed website

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'image.tmdb.org',
-                port: '',
-              },
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "vidsrc.xyz",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "vidlink.pro",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "multiembed.mov",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
